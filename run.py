c0b3756a4018b3434cbd035e70c99690c8568b95
mileswang97 <mileswang@college.harvard.edu>
2018-12-05 14:10:14 -0500

    run.py update 0.1
    reorganized method for updating games. doesn't read from csv every call

--- run.py
+++ run.py
@@ -3,26 +3,50 @@ import datetime
 import json
 import time
 import pandas as pd
+import numpy as np
 import dota2api
 api = dota2api.Initialise("4D871C3CC56D4E987911394AF8F6C021", raw_mode=True)
+
+
+live_games_array = np.empty()
+
+# When called, function updates old live game list with new.
+# Any game that gets replaced will be recorded in unparsed_output.csv
+def update_live_games():
+
+    # pull game information
+    live_games = api.get_top_live_games()
+    game_count=len(live_games['game_list'])
+    new_list = []
+    
+    # filter for mmr > 0
+    for i in range(game_count):
+        mmr = live_games['game_list'][i]['average_mmr']
+        matchid = live_games['game_list'][i]['match_id']
+        matchtime = live_games['game_list'][i]['activate_time']
+        matchinfo = [{'matchid': matchid, 'mmr': mmr, 'matchtime': matchtime}]
+        
+        if mmr > 0: 
+            new_list.append(matchinfo)
+
+    # convert to array for set comparison (assuming matchtime is unique and unchanged)
+    new_array = np.asarray(new_list)
+
+    # Return the sorted, unique values in ar1 that are not in ar2.
+    update_array = np.setdiff1d(new_array, live_games_array)
+
+    # Record new games in csv file
+    for i in range(len(update_list)):
+        pd.DataFrame(update_array[i]).to_csv('unparsed_output.csv', mode='a', header=False)
+
+    # Update array for next function call
+    live_games_array = new_array
+    print("updated since", datetime.datetime.now())
+    return
+
 while True:
         try:
-                live_games = api.get_top_live_games()
-                game_count=len(live_games['game_list'])
-                old_games = pd.read_csv("output.csv")
-                for i in range(0,game_count):
-                        mmr = live_games['game_list'][i]['average_mmr']
-                        matchid = live_games['game_list'][i]['match_id']
-                        matchtime= live_games['game_list'][i]['activate_time']
-                        matchinfo = [{'matchid': matchid, 'mmr': mmr, 'matchtime': matchtime}]
-                        ismatch = matchid in set(old_games['Match Id'])
-                        print(mmr)
-                        print(matchid in set(old_games['Match Id']))
-                        if mmr > 0:
-                                if  not ismatch:
-                                        print(matchinfo)
-                                        pd.DataFrame(matchinfo).to_csv('output.csv', mode='a', header=False)
-                print(datetime.datetime.now())
-                time.sleep(600)
+            update_live_games()
+            time.sleep(600)
         except:
             pass
