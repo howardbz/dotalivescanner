13b7d04c7c8bf5d04e891354512191f7ce046a17
mileswang97 <mileswang@college.harvard.edu>
2018-12-05 16:18:22 -0500

    run.py 0.1b
    bug fixes, streamlined process to compare sets into if statement. Should work now

--- run.py
+++ run.py
@@ -8,44 +8,45 @@ import dota2api
 api = dota2api.Initialise("4D871C3CC56D4E987911394AF8F6C021", raw_mode=True)
 
 
-live_games_array = np.zeros(1)
+live_games_list = []
 
 # When called, function updates old live game list with new.
 # Any game that gets replaced will be recorded in unparsed_output.csv
 def update_live_games():
-    print(live_games_array)
+    global live_games_list
+    print(live_games_list)
     # pull game information
     print("pulling game information")
     live_games = api.get_top_live_games()
     game_count=len(live_games['game_list'])
     new_list = []
+    update_list = []
     
-    # filter for mmr > 0
     for i in range(game_count):
         mmr = live_games['game_list'][i]['average_mmr']
         matchid = live_games['game_list'][i]['match_id']
         matchtime = live_games['game_list'][i]['activate_time']
         matchinfo = [{'matchid': matchid, 'mmr': mmr, 'matchtime': matchtime}]
-        
-        if mmr > 0: 
-            new_list.append(matchinfo)
 
-    # convert to array for set comparison (assuming matchtime is unique and unchanged)
-    new_array = np.asarray(new_list)
-    print("new array is")
-    print(new_array)
+        if mmr > 0:
+            # if matchid exists in previous array, is repeated match, retain entry in next call
+            if matchid in live_games_list:
+                new_list.append(matchid)
+            # if matchid doesn't exist, is new match, put entry in next call and record in csv
+            else:
+                pd.DataFrame(matchinfo).to_csv('unparsed_output.csv', mode='a', header=False)
+                new_list.append(matchid)
+                update_list.append(matchid)
+            # if matchid existed in previous array but isn't queried, deleted from next call automatically
 
-    # Return the sorted, unique values in ar1 that are not in ar2.
-    update_list = np.setdiff1d(new_array, live_games_array)
+    # convert to array and update live games array for next call comparison
+    live_games_list = new_list
+    print("new live games array is")
+    print(live_games_list)
+
     print("updated list is")
     print(update_list)
 
-    # Record new games in csv file
-    for i in range(len(update_list)):
-        pd.DataFrame(update_list[i]).to_csv('unparsed_output.csv', mode='a', header=False)
-
-    # Update array for next function call
-    live_games_array = new_array
     print("updated since")
     print(datetime.datetime.now())
     return
@@ -56,5 +57,9 @@ while True:
             update_live_games()
         except:
             pass
-        print("waiting for 10 seconds")
-        time.sleep(10)
+        print("waiting for 10 minutes")
+        time.sleep(600)
+
+# print("updating")
+# update_live_games(live_games_array)
+
